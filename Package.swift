7b9f53e2c24e881061be6df31a2908b6a0b1beae
Diptesh Patel <diptesh.patel@rakuten.com>
2025-08-12 09:29:26 +1000

    3.0.2

--- Package.swift
+++ Package.swift
@@ -8,38 +8,31 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         .library(
-            name: "FillrAPI",
-            targets: ["FillrAPI"]),
-        .library(
-            name: "FillrAnalytics",
-            targets: ["FillrAnalytics"]),
-        .library(
-            name: "FillrSDK",
-            targets: ["FillrSDK"]),
-        .library(
             name: "FillrAutofillSDK",
-            targets: ["FillrAutofillSDK"]),
+            targets: ["FillrAutofillSDK"],
+            dependencies: ["FillrAPI", "FillrAnalytics", "FillrSDK"]
+        )
     ],
     targets: [
         .binaryTarget(
             name: "FillrAPI",
-            url: "https://fillr.jfrog.io/artifactory/FillrSPM/dev/FillrSDK/FillrSDK-3.0.0.zip",
-            checksum: "2e247aa1187c02c8c734285b37b245ac0bff90a7a5b07b12387a048c9a47b10c",
+            url: "https://fillr.jfrog.io/artifactory/FillrSPM/dev/FillrAPI/FillrAPI-3.0.0.zip",
+            checksum: "143d4ded64ed041c38b1bc0657d5cf28f96584065439aa4660574fabaca99809"
         ),
         .binaryTarget(
             name: "FillrAnalytics",
-            url: "https://fillr.jfrog.io/artifactory/FillrSPM/dev/FillrSDK/FillrSDK-3.0.0.zip",
-            checksum: "2e247aa1187c02c8c734285b37b245ac0bff90a7a5b07b12387a048c9a47b10c",
+            url: "https://fillr.jfrog.io/artifactory/FillrSPM/dev/FillrAnalytics/FillrAnalytics-3.0.0.zip",
+            checksum: "92fd6c01125dfcf835d9baa5007b56f2724a9e57a851d0fa3e415b54a07186fd"
         ),
         .binaryTarget(
             name: "FillrSDK",
             url: "https://fillr.jfrog.io/artifactory/FillrSPM/dev/FillrSDK/FillrSDK-3.0.0.zip",
-            checksum: "2e247aa1187c02c8c734285b37b245ac0bff90a7a5b07b12387a048c9a47b10c",
+            checksum: "ce414bae7967587a6006e17f39c8e4db467274d010dbb48ed5f7d426de3c7135",
         ),
         .binaryTarget(
             name: "FillrAutofillSDK",
             url: "https://fillr.jfrog.io/artifactory/FillrSPM/dev/FillrAutofillSDK/FillrAutofillSDK-3.0.0.zip",
-            checksum: "8068fb51d54cd343463e2ed5d95e0cf05aae23ebac85a913fa32beb42bdfd375"
+            checksum: "c5a50d3d4755c2e4dcdac2f382e46fd8339e4364be63305c8fc3538d66548ea5"
         )
     ]
 )
