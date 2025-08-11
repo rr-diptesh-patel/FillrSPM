a1b418e5f3030c907b594864ac902d603eebd546
Diptesh Patel <diptesh.patel@rakuten.com>
2025-08-12 07:31:00 +1000

    3.0.1

--- Package.swift
+++ Package.swift
@@ -24,22 +24,22 @@ let package = Package(
         .binaryTarget(
             name: "FillrAPI",
             url: "https://fillr.jfrog.io/artifactory/FillrSPM/dev/FillrSDK/FillrSDK-3.0.0.zip",
-            checksum: "80732fd630ae31ac2932882ebf2ec6ed52a87607",
+            checksum: "2e247aa1187c02c8c734285b37b245ac0bff90a7a5b07b12387a048c9a47b10c",
         ),
         .binaryTarget(
             name: "FillrAnalytics",
             url: "https://fillr.jfrog.io/artifactory/FillrSPM/dev/FillrSDK/FillrSDK-3.0.0.zip",
-            checksum: "80732fd630ae31ac2932882ebf2ec6ed52a87607",
+            checksum: "2e247aa1187c02c8c734285b37b245ac0bff90a7a5b07b12387a048c9a47b10c",
         ),
         .binaryTarget(
             name: "FillrSDK",
             url: "https://fillr.jfrog.io/artifactory/FillrSPM/dev/FillrSDK/FillrSDK-3.0.0.zip",
-            checksum: "80732fd630ae31ac2932882ebf2ec6ed52a87607",
+            checksum: "2e247aa1187c02c8c734285b37b245ac0bff90a7a5b07b12387a048c9a47b10c",
         ),
         .binaryTarget(
             name: "FillrAutofillSDK",
             url: "https://fillr.jfrog.io/artifactory/FillrSPM/dev/FillrAutofillSDK/FillrAutofillSDK-3.0.0.zip",
-            checksum: "6552bc6ebaf0ddca9ab6da117cbc345e17e5a0cb"
+            checksum: "8068fb51d54cd343463e2ed5d95e0cf05aae23ebac85a913fa32beb42bdfd375"
         )
     ]
 )
